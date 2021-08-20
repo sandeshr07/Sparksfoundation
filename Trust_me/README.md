2520b9f1160ba30ff2d1f32b6cc77e35a75c59b9
sandeshr07 <sandeshputtu7gowda@gmail.com>
2021-08-20 22:00:31 +0530

    project

---
+++ Trust_me/README.md
@@ -0,0 +1,34 @@
+# The-Sparks-Foundation
+### Internship Task #1: Basic Banking System
+
+> A basic banking system
+![Preview](/static/preview.png)
+
+[LIVE DEMO](https://harshalgrow.000webhostapp.com)
+
+Project Specifications:
+
+1. A basic banking web-app used to transfer money between multiple users.
+2. The project contains 10 dummy users. A new dummy user can also be created. 
+3. It features user validation for transfer of money i.e. No negative, zero value can be transferred. 
+4. Also, if the balance of the sender is insufficient, an alert message is displayed for the same.
+5. All Transaction History is displayed to the user with timestamp. 
+
+
+Tech Stack used:
+
+* Front-end
+
+    * HTML
+    * CSS
+    * JavaScript
+    * Bootstrap
+* Back-end
+
+    * PHP
+* Database
+
+    * MySQL
+
+Website Flow:
+ Home Page > View all Users > Select and View one User > Transfer Money > Select receiver > View all Users > View Transfer History.
---
+++ Trust_me/sql/Trust_me.sql
@@ -0,0 +1,98 @@
+-- phpMyAdmin SQL Dump
+
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `grow_bank`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transaction`
+--
+
+CREATE TABLE `transaction` (
+  `sno` int(3) NOT NULL,
+  `sender` text NOT NULL,
+  `receiver` text NOT NULL,
+  `balance` int(8) NOT NULL,
+  `datetime` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `transaction`
+--
+
+INSERT INTO `transaction` (`sno`, `sender`, `receiver`, `balance`, `datetime`) VALUES
+(1, 'Vishwa', 'shash', 2000, '2021-08-13 11:46:07'),
+(2, 'Santhosh', 'sharan', 2000, '2021-08-13 11:46:42');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(3) NOT NULL,
+  `name` text NOT NULL,
+  `email` varchar(30) NOT NULL,
+  `balance` int(8) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `email`, `balance`) VALUES
+(1, 'shash', 'shash@gmail.com', 50000),
+(2, 'sharan', 'sharan@gmail.com', 40000),
+(3, 'sujay', 'sujay@gmail.com', 30000),
+(4, 'sai', 'sai@gmail.com', 35000);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `transaction`
+--
+ALTER TABLE `transaction`
+  ADD PRIMARY KEY (`sno`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `transaction`
+--
+ALTER TABLE `transaction`
+  MODIFY `sno` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
